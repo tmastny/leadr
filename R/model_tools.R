0be8e55b929360a4db2a18a6e77dfe97d9c70c7f
Timothy Mastny <tim.mastny@gmail.com>
2018-03-07 14:35:45 -0600

    removed caret_seed from docs

--- R/model_tools.R
+++ R/model_tools.R
@@ -1,27 +1,3 @@
-#' Set seed for \code{caret}'s \code{trainControl}
-#'
-#' Given a starting seed and number of folds (or resamples),
-#' this function creates a vector of seeds needed for the `caret`
-#' trainControl object.
-#'
-#' @param seed starting seed for list of seeds
-#' @param number number of folds or resamples
-#'
-#' @return List of seeds used by \code{trainControl}.
-#'
-#' @examples
-#' trainControl(method = 'cv', number = 10, seeds = caret_seed(1234, 10))
-#'
-#' @export
-caret_seed <- function(seed = 1, number) {
-  set.seed(seed)
-  seeds <- vector(mode = "list", length = number + 1)
-
-  for(i in 1:number) seeds[[i]] <- sample.int(n=1000, 4)
-  seeds[[number + 1]] <- sample.int(1000, 1)
-  seeds
-}
-
 #' Return out of fold model predictions
 #'
 #' Given a model or list of models, this function returns the
